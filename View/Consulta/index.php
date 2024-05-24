6900a25f197d0eda9faab574afaaf3939c04e7b2
Alfredo Fiestas <jfiestas650@gmail.com>
2024-05-23 20:30:38 -0500

    primer commit

--- View/Consulta/index.php
+++ View/Consulta/index.php
@@ -42,7 +42,7 @@
   <body class="pos-relative">
 
     <div class="ht-100v d-flex align-items-center justify-content-center">
-      <div class="wd-lg-70p wd-xl-50p tx-center pd-x-40">
+      <div class="wd-lg-70p wd-xl-50p tx-center pd-x-40 ">
         <h1 class="tx-100 tx-xs-140 tx-normal tx-inverse tx-roboto mg-b-0">Diplomas y Certificado</h1>
         <h5 class="tx-xs-24 tx-normal tx-info mg-b-30 lh-5">Ingrese DNI para Validar Certificados</h5>
 
